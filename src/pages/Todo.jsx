556b7f7f1a6df1f14b4eb723cab344bafe76910b
saeed <saeidsaued@gmail.com>
2024-05-06 16:50:44 +0300

    add lottie animation

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,8 +4,10 @@ import { useContextProvider } from "../context/ContextProvider";
 // components
 import { CardBody, PageHeader, TodoItem } from "../components";
 import { AnimatePresence } from "framer-motion";
+import Lottie from "lottie-react";
 // icons
 import { IoMdAdd } from "react-icons/io";
+import ghost from "../assets/animations/ghost.json";
 
 const TodoCard = ({ children, title }) => (
   <div className="flex-1">
@@ -65,7 +67,9 @@ const Todo = () => {
       </div>
       <div className="w-full gap-6 hidden md:flex">
         <TodoCard title={"in progress"}>
-          {!todoData.filter((todo) => !todo.isDone).length && <p>no data</p>}
+          {!todoData.filter((todo) => !todo.isDone).length && (
+            <Lottie animationData={ghost} className="h-2/3" />
+          )}
           {todoData.map(
             (todo) =>
               !todo.isDone && (
@@ -80,6 +84,8 @@ const Todo = () => {
           )}
         </TodoCard>
         <TodoCard title={"important"}>
+          {!todoData.filter((todo) => !todo.isDone && todo.isImportant)
+            .length && <Lottie animationData={ghost} className="h-2/3" />}
           {todoData.map(
             (todo) =>
               !!todo.isImportant &&
@@ -95,6 +101,9 @@ const Todo = () => {
           )}
         </TodoCard>
         <TodoCard title={"completed"}>
+          {!todoData.filter((todo) => todo.isDone).length && (
+            <Lottie animationData={ghost} className="h-2/3" />
+          )}
           {todoData.map(
             (todo) =>
               !!todo.isDone && (
@@ -124,6 +133,14 @@ const Todo = () => {
             </select>
           }
         >
+          {!todoData.filter(
+            (todo) =>
+              (filterTodoKey == "in progress" && !todo.isDone) ||
+              (filterTodoKey == "important" &&
+                !!todo.isImportant &&
+                !todo.isDone) ||
+              (filterTodoKey == "completed" && !!todo.isDone)
+          ).length && <Lottie animationData={ghost} className="h-full" />}
           {todoData
             .filter(
               (todo) =>
